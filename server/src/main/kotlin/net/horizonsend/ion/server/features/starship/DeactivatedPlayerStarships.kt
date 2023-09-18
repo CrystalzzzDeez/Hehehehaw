3c426ca159ab5ad4d9b70afe95046a275018a373
Gutin1 <Claytonpinter@gmail.com>
2023-09-18 02:25:50 -0500

    rename some functions, fix some async issues, debug attempts

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/DeactivatedPlayerStarships.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/DeactivatedPlayerStarships.kt
@@ -86,7 +86,7 @@ object DeactivatedPlayerStarships : IonServerComponent() {
 				isLockEnabled = true
 			)
 
-			createPlayerShipAsync(world, x, y, z, data, callback)
+			createShipAsync(world, x, y, z, data, callback)
 		}
 	}
 
@@ -112,11 +112,11 @@ object DeactivatedPlayerStarships : IonServerComponent() {
 				name = name
 			)
 
-			Tasks.sync { callback(data) }
+			createShipAsync(world, x, y, z, data, callback)
 		}
 	}
 
-	fun createPlayerShipAsync(
+	private fun createShipAsync(
 		world: World,
 		x: Int,
 		y: Int,
@@ -202,6 +202,8 @@ object DeactivatedPlayerStarships : IonServerComponent() {
 		val cache = DeactivatedShipWorldCache(world)
 		// retrieve all starship data from the database and add it to the cache
 		PlayerStarshipData.find(PlayerStarshipData::levelName eq world.name).forEach { cache.add(it) }
+		AIStarshipData.find(AIStarshipData::levelName eq world.name).forEach { cache.add(it) }
+
 		DEACTIVATED_SHIP_WORLD_CACHES[world] = cache
 	}
 
@@ -235,7 +237,7 @@ object DeactivatedPlayerStarships : IonServerComponent() {
 			val carriedShipMap = captureCarriedShips(carriedShips, cache)
 
 			Tasks.sync {
-				val starship = ActiveStarshipFactory.createPlayerStarship(
+				val starship = ActiveStarshipFactory.createControlledStarship(
 					feedbackDestination,
 					data,
 					state.blockMap.keys,
