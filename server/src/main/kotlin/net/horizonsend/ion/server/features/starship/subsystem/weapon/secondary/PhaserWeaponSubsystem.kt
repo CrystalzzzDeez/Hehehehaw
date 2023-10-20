7efd9c16dda8093aa4fe87d23c756a91b18aaa9a
Crystalzzz <116468614+CrystalzzzDeez@users.noreply.github.com>
2023-10-20 18:11:16 +0100

    implement real cooldow

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/secondary/PhaserWeaponSubsystem.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/secondary/PhaserWeaponSubsystem.kt
@@ -37,8 +37,8 @@ class PhaserWeaponSubsystem(
 
 	override fun fire(loc: Location, dir: Vector, shooter: Damager, target: Vector?) {
     		if (starship.initialBlockCount > 12000) {
-			shooter.userError("You can't fire phasers on a ship larger than 12000 blocks!")
-			return
+				shooter.userError("You can't fire phasers on a ship larger than 12000 blocks!")
+				return
 		}
 
 		fixDirections(loc)
